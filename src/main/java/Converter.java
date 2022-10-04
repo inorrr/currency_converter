bc29d29c499c60048268b5644e75ce5d443ab88b
inorrr <inor.zhao@mail.utoronto.ca>
2022-10-04 16:26:38 -0400

    Initial commit

---
+++ src/main/java/Converter.java
@@ -0,0 +1,7 @@
+public class converter {
+
+
+    public float convert(String form, String to, float value){
+
+    }
+}
---
+++ src/main/java/DataLoader.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.List;
+import java.io.*;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+
+public class DataLoader {
+
+    public DataLoader(){}
+
+    public HashMap loadExchangeRate(){
+        try
+    }
+
+
+
+}
